efea6e774449018fd5fd5910123c502f60ba4818
Diego-World <contato.diegogouveia@gmail.com>
2023-11-15 13:29:49 -0300

    feat: modelagem de dados e comportamentos da classe Tabuleiro

--- src/main/java/br/com/diegogouveia/app/modelo/Tabuleiro.java
+++ src/main/java/br/com/diegogouveia/app/modelo/Tabuleiro.java
@@ -1,5 +1,39 @@
 package br.com.diegogouveia.app.modelo;
 
-public class Tabuleiro {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Tabuleiro {
+    private int linhas;
+    private int colunas;
+    private int minas;
+    private final List<Campo> campos = new ArrayList<>();
+    public Tabuleiro(){
+
+    }
+    public Tabuleiro(int linhas, int colunas, int minas) {
+        this.linhas = linhas;
+        this.colunas = colunas;
+        this.minas = minas;
+
+        gerarCampos();
+        associarVizinhos();
+        sortearMinas();
+    }
+    private void gerarCampos() {
+        for (int linha = 0; linha < linhas; i++) {
+            for (int coluna = 0; coluna < colunas; j++) {
+                campos.add(new Campo(linha,coluna));
+            }
+        }
+    }
+    private void associarVizinhos() {
+        for (Campo c1: campos) {
+            for (Campo c2:campos) {
+                c1.adicionarVizinho(c2);
+            }
+        }
+    }
+    private void sortearMinas() {
+    }
 }
