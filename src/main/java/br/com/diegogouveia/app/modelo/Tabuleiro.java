7dda499c0e4fca60fbcddbf3ba63c784d9e16e21
Diego-World <contato.diegogouveia@gmail.com>
2023-11-17 10:04:30 -0300

    feat: comportamentos do tabuleiro e construção da GUI

--- src/main/java/br/com/diegogouveia/app/modelo/Tabuleiro.java
+++ src/main/java/br/com/diegogouveia/app/modelo/Tabuleiro.java
@@ -1,7 +1,10 @@
 package br.com.diegogouveia.app.modelo;
 
+import br.com.diegogouveia.app.exceptions.ExplosaoException;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Tabuleiro {
     private int linhas;
@@ -20,9 +23,30 @@ public class Tabuleiro {
         associarVizinhos();
         sortearMinas();
     }
+
+    public void abrir(int linha, int coluna){
+
+        try{
+            campos.parallelStream()
+                    .filter(c -> c.getLinha() == linha && c.getColuna() == coluna)
+                    .findFirst()
+                    .ifPresent(campo -> campo.abrir());
+        }catch (ExplosaoException e){
+            campos.forEach( c-> c.setAberto(true));
+            throw e;
+        }
+    }
+
+    public void alternarMarcacao(int linha, int coluna){
+        campos.parallelStream()
+                .filter(c -> c.getLinha() == linha && c.getColuna() == coluna)
+                .findFirst()
+                .ifPresent(campo -> campo.alternarMarcacao());
+    }
+
     private void gerarCampos() {
-        for (int linha = 0; linha < linhas; i++) {
-            for (int coluna = 0; coluna < colunas; j++) {
+        for (int linha = 0; linha < linhas; linha++) {
+            for (int coluna = 0; coluna < colunas; coluna++) {
                 campos.add(new Campo(linha,coluna));
             }
         }
@@ -35,5 +59,58 @@ public class Tabuleiro {
         }
     }
     private void sortearMinas() {
+        long minasArmadas = 0;
+        Predicate<Campo> minado = c -> c.isMinado();
+        do{
+            int aleatorio = (int) (Math.random() * campos.size());
+            campos.get(aleatorio).minar();
+            minasArmadas = campos.stream().filter(minado).count();
+        }while (minasArmadas < minas);
     }
+
+    public int getMinas() {
+        return minas;
+    }
+
+    public void setMinas(int minas) {
+        this.minas = minas;
+    }
+
+    public boolean objeivoAlcancado(){
+        return campos.stream().allMatch(campo -> campo.objeivoAlcancado());
+    }
+
+    public void reiniciarJogo(){
+        campos.stream().forEach(campo -> campo.reiniciar());
+        sortearMinas();
+    }
+
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(" ");
+        for (int c = 0; c < colunas; c++) {
+            sb.append(" ");
+            sb.append(c);
+            sb.append(" ");
+        }
+
+        sb.append("\n");
+
+        int i = 0;
+        for (int l = 0; l < linhas; l++) {
+            sb.append(l);
+            for(int c = 0; c < colunas ; c++){
+                sb.append(" ");
+                sb.append(campos.get(i));
+                sb.append(" ");
+                i++;
+            }
+            sb.append("|");
+            sb.append("\n");
+        }
+
+        return sb.toString();
+    }
+
 }
